3147fad3567a173b8f48580a4c3d2f17ab3ebdcb
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2017-07-25 11:03:05 +0100

    adds in interactive designer

---
+++ app/assets/javascripts/designer/elements.class.js
@@ -0,0 +1,49 @@
+GOVUK.PrototypeDesigner = GOVUK.PrototypeDesigner != null ? GOVUK.PrototypeDesigner : {};
+GOVUK.PrototypeDesigner.Elements = class
+{
+    constructor()
+    {
+        this.templateEngine = GOVUK.PrototypeDesigner.Templates;
+        this.initListeners();
+    }
+
+    initListeners()
+    {
+        this.initFields();
+    }
+
+    initFields()
+    {
+        var fieldTypes = [
+            'input',
+            'textarea',
+            'select',
+            'radio',
+            'checkbox',
+            'upload'
+        ];
+
+        for( var i in fieldTypes )
+        {
+            this.initFieldListener( fieldTypes[i] );
+        }
+    }
+
+    initFieldListener( fieldType=null )
+    {
+        if ( fieldType === null) return;
+
+        var template = this.templateEngine.getTemplate(fieldType);
+        
+        $(`.prototype-elements #add_${fieldType}`).on('click', function(){
+            $('.prototype-preview form').append( template );
+        });
+
+        return;
+    }
+}
+
+new GOVUK.PrototypeDesigner.Elements();
+
+
+
