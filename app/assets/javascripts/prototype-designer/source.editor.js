8ec5afdd2fcfcf46b0d1ce47acd643c6b5d595fb
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2017-09-27 17:30:32 +0100

    updates

--- app/assets/javascripts/prototype-designer/source.editor.js
+++ app/assets/javascripts/prototype-designer/source.editor.js
@@ -7,16 +7,16 @@ class sourceEditor
 
     init()
     {
-      var editor = ace.edit("editor");
-      editor.setTheme("ace/theme/monokai");
-      editor.session.setMode("ace/mode/html");
-      editor.on('change', function(){
-        var value = editor.getSession().getValue();
-        $('#prototypeCanvas').html(value);
-      });
-      $.get('/public/lib/ace-editor/default.template').done(function(result){
-        editor.setValue(result);
-      })
+        var editor = ace.edit("editor");
+        editor.setTheme("ace/theme/monokai");
+        editor.session.setMode("ace/mode/html");
+        editor.on('change', function(){
+          var value = editor.getSession().getValue();
+          $('#prototypeCanvas').html(value);
+        });
+        $.get('/public/lib/ace-editor/default.template').done(function(result){
+          editor.setValue(result);
+        })
     }
 
 }
